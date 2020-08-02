d1f6e47fa17a3e4c18ada92e37d5a19817625d63
khedubaba <adityakhedekar49@gmail.com>
2020-08-02 18:20:24 +0530

    Extracting body of incoming request using body-parser package and hancling CORS erros

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     });
 });
 
--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//CORS headers to avoid CORS erros
+//Appending headers to funneel every request throught it to avoid CORS errors
+app.use((req, res, next) => {
+    //defeining allow origin header to allow access from certain or all (*) origins
+    res.header('Access-control-Allow-Origin', '*');
+
+    //defining which kind of headers to be appende to incoming requests *-all headers type
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//Error handling
 app.use((req, res, next) => {
     const error = new Error('Not dound');
     error.status = 404;
